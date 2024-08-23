ce18631b5096ebb2970dd099ccf09252f1e80958
vasanth <vasanthrv84@gmail.com>
2024-08-23 09:31:13 +0530

    aug 22 commit

---
+++ Aug22/AnonymousTypeDemo/AnonymousTypeDemo/Program.cs
@@ -0,0 +1,52 @@
+﻿class Sample
+{
+    public Tuple<string, int> GetPersonDetails()
+    {
+        return new Tuple<string, int>("bob", 24);
+    }
+}
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var person = new { Name = "tim", Age = 21 }; // Anonymous type
+        //Console.WriteLine(person.Name + " is " + person.Age + " years old");
+
+        var person2 = new { Name = "John", Age = 22, Address = new { flat = 2, city = "tvl" } }; //Nested anonymous type
+        //Console.WriteLine(person2.Name + " is " + person2.Age + " years old " + "residing at flat " + person2.Address.flat + " in " + person2.Address.city);
+
+        var personList = new[]
+        {
+            new { Name = "tim", Age = 21 },
+            new { Name = "bob", Age = 25 },
+            new { Name = "alex", Age = 30 }
+        };
+
+        foreach (var x in personList)
+        {
+            //Console.WriteLine(x.Name + " is " + x.Age + " years old");
+        }
+
+        Tuple<string, int> p1 = new Tuple<string, int>("scott", 20);
+        //p1.Item1 = "anderw"; tuple is read only
+        //Console.WriteLine(p1.Item1);
+        //Console.WriteLine(p1.Item2);
+
+        //value tuple
+        (int customerId, string customerName, string email) customer = (101, "Babu", "babu@gmail.com");
+        Console.WriteLine(customer.customerId + "   " + customer.customerName + "   " + customer.email);
+
+        //Deconstruction
+        (int id, string name, string email) = customer;
+        Console.WriteLine(id + "  " + name + "  " + email);
+
+        //Discard
+        (_, string customerName, _) = customer;
+        Console.WriteLine(customerName);
+
+        Console.WriteLine("william" == "william");
+    }
+}
---
+++ Aug22/CollectionDemo1/CollectionDemo1/Program.cs
@@ -0,0 +1,72 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> list = new List<int>(100) { 10, 20, 30 };
+        list.Capacity = 12;
+        //Console.WriteLine(list.Capacity);
+        list.Add(40);
+
+        List<int> list2 = new List<int> { 50, 60, 70 };
+        list.AddRange(list2);
+
+        list.Remove(20);
+        //list.RemoveAll(n => n > 30);
+
+        list.Insert(1, 100);
+        list.InsertRange(2, list2);
+        //foreach (int i in list)
+        //{
+        //    Console.WriteLine(i);
+        //}
+
+        Console.WriteLine(list.IndexOf(100, 3));
+        Console.WriteLine(list.IndexOf(100));
+
+        int[] array = list.ToArray();
+
+        string[] name = { "tim", "john", "ben", "jack" };
+        List<string> nameList = name.ToList();
+        nameList.ForEach(
+            x => { Console.WriteLine(x); }
+            );
+
+        List<int> marks = new List<int>() { 30, 95, 24, 70, 61, 81 };
+
+        //bool b = marks.Exists(m => m < 35);
+        //if (b) Console.WriteLine("failed");
+        //else Console.WriteLine("passed");
+
+        //List<int> failedMarks = marks.FindAll(n => n < 35);
+        //Console.WriteLine("Failed marks : ");
+        //failedMarks.ForEach(x => Console.WriteLine(x));
+
+        Dictionary<int, string> dictionary = new Dictionary<int, string>()
+        {
+            { 101, "Scott" },
+            { 102, "Smith" },
+            { 103, "Allen" }
+        };
+        dictionary.Add(104, "ben");
+        foreach (KeyValuePair<int, string> item in dictionary)
+        {
+            Console.WriteLine(item.Key + "   " + item.Value);
+        }
+        Console.WriteLine(dictionary[101]);
+        Console.WriteLine(dictionary.ContainsKey(1001));
+
+        SortedList<int, string> sortedList = new SortedList<int, string>()
+        {
+            { 103, "Scott" },
+            { 102, "Smith" },
+            { 1010, "Allen" }
+        };
+        sortedList.Add(1, "ben");
+        foreach (KeyValuePair<int, string> item in sortedList)
+        {
+            Console.WriteLine(item.Key + "   " + item.Value);
+        }
+
+
+    }
+}
